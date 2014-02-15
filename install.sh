0d3af56fd7d719ba3f82695b22704488fc264c7e
ben hockey <neonstalwart@gmail.com>
2014-02-14 20:28:19 -0600

    install: get the order right for oh-my-zsh

--- install.sh
+++ install.sh
@@ -10,12 +10,8 @@ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # clone dotfiles
 echo "cloning .dotfiles"
-git clone https://github.com/neonstalwart/.dotfiles.git ~/.dotfiles
+git clone --recursive https://github.com/neonstalwart/.dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
-
-# oh-my-zsh
-echo "installing oh-my-zsh"
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # install brew bundle from Brewfile
 echo "installing brew bundle"
@@ -23,17 +19,23 @@ brew bundle
 
 # install nvm
 echo "installing nvm"
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+curl -L https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# set paths for brew installs
+# initialize nvm
 source zshenv
-
-# change shells
-echo "changing shell to zsh"
-ZSHPATH=$(which zsh)
-sudo -s "echo $ZSHPATH >> /etc/shell"
-chsh -s $ZSHPATH
 
 # install node
 echo "installing node"
 nvm install 0.10
+
+# add zsh to etc/shells
+echo "adding $(which zsh) to /etc/shells"
+sudo -s "echo $(which zsh) >> /etc/shells"
+
+# oh-my-zsh
+echo "installing oh-my-zsh"
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# install zsh startup files
+cp zshenv ~/.zshenv
+cp zshrc ~/.zshrc
