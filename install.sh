dfecf2b3d0fd8d9828f1184ac8ccf47ccb342709
ben hockey <neonstalwart@gmail.com>
2014-05-30 11:29:50 -0500

    tmux: better keybindings and theming

--- install.sh
+++ install.sh
@@ -45,6 +45,7 @@ ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux ~/.tmux
 ln -s ~/.dotfiles/agignore ~/.agignore
 
 # change shells
